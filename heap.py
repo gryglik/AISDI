581b25410bce5ee9905e6f4d585ddef1a8620583
Michał Gryglicki <michal.gryglicki2.stud@pw.edu.pl>
2024-04-02 13:57:21 +0200

    Refactorised program added class HEAP

--- heap.py
+++ heap.py
@@ -1,33 +1,62 @@
-def heapify(k: int, n: int, T: list[int]) -> None:
-    length = len(T) - 1
-    left = (n - 1) * k + 2 if (n - 1) * k + 2 <= length else None
-    right = n * k + 1 if n * k + 1 <= length else length
+class Heap():
+    def __init__(self, a: int = 2, T: list[int | None] = []) -> None:
+        self._a: int = a
+        if T[0]:
+            self._T: list[int | None] = [None] + T[:]
+        else:
+            self._T: list[int | None] = T[:]
+        self._make_heap()
 
-    if left:
-        i_max = left
-        for i in range(left + 1, right + 1):
-            if T[i] > T[i_max]:
-                i_max = i
-        if T[i_max] > T[n]:
-            temp = T[n]
-            T[n] = T[i_max]
-            T[i_max] = temp
-            heapify(k, i_max, T)
+    def list(self) -> list[int | None]:
+        return self._T[:]
 
+    def len(self) -> int:
+        return len(self._T) - 1 # bez None
 
-def make_heap(k: int, T: list[int]) -> None:
-    length = len(T) - 1
-    for i in range(1 + (length - 2) // k, 0, -1):
-        heapify(k, i, T)
+    def right(self, n: int) -> int | None:
+        # ostatnie dziecko danego rodzica znajduje się pod indeksem
+        # n * a + 1 - przesunięcie w prawo
+        right = n * self._a + 1 if n * self._a + 1 <= self.len() else self.len()
+        return right if right <= self.len() else None
 
+    def left(self, n: int) -> int | None:
+        # pierwsze dziecko danego rodzica znajduje się pod indeksem
+        # right() - (a - 1) = n * a + 1 - (a - 1) = (n - 1) * a + 2
+        return (n - 1) * self._a + 2 if (n - 1) * self._a + 2 <= self.len() else None
 
-def add(k: int, new_item: int, T: list[int]) -> None:
-    T.append(new_item)
-    length = len(T) - 1
-    i_item = length
-    i_parent = 1 + (i_item - 2) // k
-    while T[i_item] > T[i_parent] and not i_item == 1:
-        T[i_item] = T[i_parent]
-        T[i_parent] = new_item
-        i_item = i_parent
-        i_parent = 1 + (i_item - 2) // k
+    def parent(self, n: int) -> int | None:
+        if n == 1:
+            return None
+        # indeksy dzieci [(x - 1) * a + 2, x * a + 1], więc aby uzyskać
+        # indeks rodzica x należy przesunąć indeksy dzieci o +(a - 2)
+        # wtedy przesunięte indeksy dzieci [x * a, (x + 1) * a - 1]
+        # dzielone przez stopień kopca (a) daje indeks rodzica (x)
+        return (n + self._a - 2) // self._a
+
+    def _swap(self, i: int, j: int) -> None:
+        temp = self._T[i]
+        self._T[i] = self._T[j]
+        self._T[j] = temp
+
+    def _heapify(self, n: int) -> None:
+        if self.left(n):
+            i_max = self.left(n)
+            for i in range(self.left(n) + 1, self.right(n) + 1):
+                if self._T[i] > self._T[i_max]:
+                    i_max = i
+            if self._T[i_max] > self._T[n]:
+                self._swap(i_max, n)
+                self._heapify(i_max)
+
+    def _make_heap(self) -> None:
+        for i in range(1 + (self.len() - 2) // self._a, 0, -1):
+            self._heapify(i)
+
+    def add(self, new_item: int) -> None:
+        self._T.append(new_item)
+        i_item = self.len()
+        i_parent = self.parent(i_item)
+        while i_parent and self._T[i_item] > self._T[i_parent]:
+            self._swap(i_item, i_parent)
+            i_item = i_parent
+            i_parent = self.parent(i_item)
--- test_heap.py
+++ test_heap.py
@@ -1,80 +1,23 @@
-from heap import heapify, make_heap, add
+from heap import Heap
 
 
 def test_heapify_2_a_typical():
-    T = [None, 1, 2, 3, 7, 8, 9, 4, 5, 10, 12]
-    heapify(2, 3, T)
-    assert T == [None, 1, 2, 9, 7, 8, 3, 4, 5, 10, 12]
-
-
-def test_heapify_2_a_leaf():
-    T = [None, 1, 2, 3, 7, 8, 9, 4, 5, 10, 12]
-    heapify(2, 6, T)
-    heapify(2, 7, T)
-    heapify(2, 8, T)
-    heapify(2, 9, T)
-    heapify(2, 10, T)
-    assert T == [None, 1, 2, 3, 7, 8, 9, 4, 5, 10, 12]
-
-
-def test_heapify_2_a_incomplete():
-    T = [None, 1, 2, 3, 7, 8, 9, 4, 5, 10, 12]
-    heapify(2, 5, T)
-    assert T == [None, 1, 2, 3, 7, 12, 9, 4, 5, 10, 8]
+    heap = Heap(2, [1, 2, 3, 7, 8, 9, 4, 5, 10, 12])
+    assert heap.list() == [None, 12, 10, 9, 7, 8, 3, 4, 5, 1, 2]
 
 
 def test_heapify_5_a_typical():
-    T = [None, 1, 2, 3, 7, 15, 6, 20, 18, 19, 8, 9, 4, 5, 10, 12]
-    heapify(5, 2, T)
-    assert T == [None, 1, 20, 3, 7, 15, 6, 2, 18, 19, 8, 9, 4, 5, 10, 12]
-
-
-def test_heapify_5_a_leaf():
-    T = [None, 1, 2, 3, 7, 15, 6, 20, 18, 19, 8, 9, 4, 5, 10, 12]
-    heapify(5, 15, T)
-    heapify(5, 4, T)
-    heapify(5, 6, T)
-    heapify(5, 12, T)
-    assert T == [None, 1, 2, 3, 7, 15, 6, 20, 18, 19, 8, 9, 4, 5, 10, 12]
-
-
-def test_heapify_5_a_incomplete():
-    T = [None, 1, 2, 3, 7, 15, 6, 20, 18, 19, 8, 9, 4, 5, 10, 12]
-    heapify(5, 3, T)
-    assert T == [None, 1, 2, 12, 7, 15, 6, 20, 18, 19, 8, 9, 4, 5, 10, 3]
+    heap = Heap(5, [1, 2, 3, 7, 8, 9, 4, 5, 10, 12])
+    assert heap.list() == [None, 12, 10, 3, 7, 8, 9, 4, 5, 1, 2]
 
 
 def test_heapify_7_a_typical():
-    T = [None, 1, 2, 3, 7, 15, 6, 20, 18, 19, 8, 9, 4, 5, 10, 12, 31]
-    heapify(7, 2, T)
-    assert T == [None, 1, 19, 3, 7, 15, 6, 20, 18, 2, 8, 9, 4, 5, 10, 12, 31]
-
-
-def test_heapify_7_a_leaf():
-    T = [None, 1, 2, 3, 7, 15, 6, 20, 18, 19, 8, 9, 4, 5, 10, 12, 31]
-    heapify(7, 4, T)
-    heapify(7, 8, T)
-    heapify(7, 9, T)
-    heapify(7, 15, T)
-    heapify(7, 16, T)
-    assert T == [None, 1, 2, 3, 7, 15, 6, 20, 18, 19, 8, 9, 4, 5, 10, 12, 31]
-
-
-def test_heapify_7_a_incomplete():
-    T = [None, 1, 2, 3, 7, 15, 6, 20, 18, 19, 8, 9, 4, 5, 10, 12, 31]
-    heapify(7, 3, T)
-    assert T == [None, 1, 2, 31, 7, 15, 6, 20, 18, 19, 8, 9, 4, 5, 10, 12, 3]
-
-
-def test_make_heap_2_a_typical():
-    T = [None, 1, 2, 3, 7, 8, 9, 4, 5, 10, 12]
-    make_heap(2, T)
-    assert T == [None, 12, 10, 9, 7, 8, 3, 4, 5, 1, 2]
+    heap = Heap(7, [1, 2, 3, 7, 15, 6, 20, 18, 19, 8, 9, 4, 5, 10, 12, 31])
+    assert heap.list() == [None, 31, 19, 3, 7, 15, 6, 20, 18, 2, 8, 9, 4, 5, 10, 12, 1]
 
 
 def test_add_2_a_typical():
-    T = [None, 1, 2, 3, 7, 8, 9, 4, 5, 10, 12]
-    make_heap(2, T)
-    assert T == [None, 12, 10, 9, 7, 8, 3, 4, 5, 1, 2]
-    add(2, 11, T)
-    assert T == [None, 12, 11, 9, 7, 10, 3, 4, 5, 1, 2, 8]
+    heap = Heap(2, [1, 2, 3, 7, 8, 9, 4, 5, 10, 12])
+    assert heap.list() == [None, 12, 10, 9, 7, 8, 3, 4, 5, 1, 2]
+    heap.add(11)
+    assert heap.list() == [None, 12, 11, 9, 7, 10, 3, 4, 5, 1, 2, 8]
