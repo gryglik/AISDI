fe65d632aca3c3a77418fdfafbd2442ed118aab2
Zuzanna Waszczuk <zuza.waszczuk@gmail.com>
2024-04-18 23:49:59 +0200

    Implemented bst and remove function

--- bst.py
+++ bst.py
@@ -1,84 +1,69 @@
 class Node():
-    def __init__(self, parent: int, left: int | None,
-                 right: int | None, key: int) -> None:
-        self.parent = parent
-        self.left = left
-        self.right = right
+    def __init__(self, key: int) -> None:
+        self.left = None
+        self.right = None
         self.key = key
 
 
 class BST():
     def __init__(self, keys: list[int] = []) -> None:
         self.nodes: list[Node | None] = []
+        self.root = None
         for key in keys:
-            self.add(key)
+            self.add(self.root, key)
 
-    def add(self, key):
-        if self.size() == 0:
-            root = Node(0, None, None, key)
-            self.nodes.append(root)
+    def add_key(self, key):
+        self.add(self.root, key)
+
+    def add(self, node: Node, key) -> Node:
+        if self.root is None:
+            self.root = Node(key)
+        if node is None:
+            return Node(key)
         else:
-            curr_node = self.nodes[0]
-            new = Node(0, None, None, key)
-            while (curr_node.parent is not None
-                   and curr_node.parent < self.size()):
-                if new.key < curr_node.key:
-                    # Our key is smaller so we go to the left child
-                    if curr_node.left:
-                        # Left exists = we go to left and checks his children
-                        new.parent = curr_node.left
-                        curr_node = self.nodes[curr_node.left]
-                    else:
-                        # Left doesnt exist - we create left from new key
-                        curr_node.left = self.size()
-                        self.nodes.append(new)
-                        break
-                else:
-                    # Our key is bigger so we go to the right child
-                    if curr_node.right:
-                        # if right exist we go to right in recursion
-                        new.parent = curr_node.right
-                        curr_node = self.nodes[curr_node.right]
-                    else:
-                        # if right doesnt exist we create right with new node
-                        curr_node.right = self.size()
-                        self.nodes.append(new)
-                        break
+            if key == node.key:
+                return node
+            elif node.key < key:
+                node.right = self.add(node.right, key)
+            else:
+                node.left = self.add(node.left, key)
+        return node
 
-    def search(self, key) -> int | None:
-        if self.size() == 0:
-            return None
+    def search(self, node, key) -> int | None:
+        if node is None or node.key == key:
+            return node
+
+        if node.key < key:
+            return self.search(node.right, key)
+
+        return self.search(node.left, key)
+
+    def remove(self, node, key):
+        if node is None:
+            return node
+
+        if key < node.key:
+            node.left = self.remove(node.left, key)
+        elif key > node.key:
+            node.right = self.remove(node.right, key)
         else:
-            curr_node = self.nodes[0]
-            if curr_node.key == key:
-                return 0
+            if node.left is None:
+                return node.right
+            elif node.right is None:
+                return node.left
 
-            while (curr_node.parent is not None
-                   and curr_node.parent < self.size()):
-                # Go left
-                if key < curr_node.key:
-                    print(f'go right {key}')
-                    # Checks if left exist
-                    if curr_node.left is None:
-                        return None
-                    if self.nodes[curr_node.left].key == key:
-                        return curr_node.left
-                    else:
-                        curr_node = self.nodes[curr_node.left]
-                # Go right
-                else:
-                    print(f'go left {key}')
-                    # Checks if right exist
-                    if curr_node.right is None:
-                        return None
-                    if self.nodes[curr_node.right].key == key:
-                        return curr_node.right
-                    else:
-                        curr_node = self.nodes[curr_node.right]
-        return None
+            node.key = self.smaller_child(node.right)
+
+            node.right = self.remove(node.right, node.key)
+
+        return node
+
+    def smaller_child(self, node):
+        child = node.key
+        while node.left:
+            child = node.left.key
+            node = node.left
+        return child
 
     def size(self) -> int:
         return len(self.nodes)
-
-    def key_list(self) -> list[int]:
-        return [node.key for node in self.nodes]
