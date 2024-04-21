59bad20f438bde2caa38b7a0b21c01d1f1dc8267
Michał Gryglicki <michal.gryglicki2.stud@pw.edu.pl>
2024-04-22 01:01:36 +0200

    minor changes

--- main.py
+++ main.py
@@ -6,5 +6,6 @@ avl_root = AVLNode()
 avl_root = avl_root.add_list([3, 6, 13, 20, 1, 23, 7, 8, 9, 10, 22, 21])
 avl_root.print()
 
-my_tree = BST([8, 2, 9, 4, 5, 11, 15, 1])
+my_tree = BST([3, 6, 13, 20, 1, 23, 7, 8])
 my_tree.print()
+
