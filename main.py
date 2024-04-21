bbda130cbc69daf4b0b1fc2953ceb74786757d96
Michał Gryglicki <michal.gryglicki2.stud@pw.edu.pl>
2024-04-22 00:14:43 +0200

    minor changes

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from avl import AVLNode
 
 
 avl_root = AVLNode()
-avl_root = avl_root.add_list([1, 2, 3, 4, 5, 6, 7, 8, 9])
+avl_root = avl_root.add_list([3, 6, 13, 20, 1, 23, 7, 8, 9, 10, 22, 21])
 avl_root.print()
